117226c3cda4fae6ec7a414b761730256482f3b1
JasonWu <1609adm@gmail.com>
2022-08-07 00:16:01 +0800

    first commit

---
+++ modinfo.lua
@@ -0,0 +1,24 @@
+-- 名称
+name = "More chinese food"
+-- 描述
+description = "更多中餐"
+-- 作者
+author = "JasonWu"
+-- 版本
+version = "0.1"
+-- klei官方论坛地址，为空则默认是工坊的地址
+forumthread = ""
+-- modicon 下一篇再介绍怎么创建的
+-- icon_atlas = "images/modicon.xml"
+-- icon = "modicon.tex"
+-- dst兼容
+dst_compatible = true
+-- 是否是客户端mod
+client_only_mod = true
+-- 是否是所有客户端都需要安装
+all_clients_require_mod = false
+-- 饥荒api版本，固定填10
+api_version = 10
+
+-- mod的配置项，后面介绍
+configuration_options = {}
